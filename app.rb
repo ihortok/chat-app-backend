afb14330587ccc43bc8cf937fa1de2325150a234
ihortok <itokaryk@gmail.com>
2025-03-28 17:43:00 +0100

    models

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "sinatra"
 require "sinatra/activerecord"
 require "json"
 require "dotenv/load"
+require_relative "./models/user"
+require_relative "./models/message"
 
 set :database_file, "config/database.yml"
 
